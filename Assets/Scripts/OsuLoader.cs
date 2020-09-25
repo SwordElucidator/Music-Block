a86c041fe05a762b9fd0495325a01e786079ea2c
Hotaru <nagisa940216@gmail.com>
2020-09-25 15:14:55 +0800

    fix the items

---
+++ Assets/Scripts/OsuLoader.cs
@@ -0,0 +1,70 @@
+﻿
+
+using System;
+using System.IO;
+
+public struct SongInfo
+{
+    // public string AudioFilename;
+    // public int AudioLeadIn;  // Milliseconds of silence before the audio starts playing
+    // public int PreviewTime;  // Time in milliseconds when the audio preview should start
+    // public int Mode;
+    public string Title;
+    public string TitleUnicode;
+    public string Artist;
+    public string ArtistUnicode;
+    public string Version;
+    public string Source;
+    public string[] Tags;
+    public int CircleSize;
+    public int ApproachRate;
+    public int BeatmapID;
+    public int BeatmapSetID;
+}
+
+
+public class OsuLoader
+{
+    private readonly FileInfo _file;
+    
+    public OsuLoader(FileInfo file)
+    {
+        _file = file;
+    }
+    
+
+    public SongInfo ReadSongInfo()
+    {
+        var reader = _file.OpenText();
+        var block = "";
+        var info = new SongInfo();
+        while (!reader.EndOfStream)
+        {
+            var line = reader.ReadLine();
+            if (line == null) continue;
+            if (line.Length >= 2 && line[0] == '[' && line[line.Length - 1] == ']')
+            {
+                block = line;
+                if (block == "[Events]") break;
+                continue;
+            }
+            if (block == "[Metadata]")
+            {
+                if (line.StartsWith("Title:")) info.Title = line.Split(new char[]{':'}, 2)[1];
+                if (line.StartsWith("TitleUnicode:")) info.TitleUnicode = line.Split(new char[]{':'}, 2)[1];
+                if (line.StartsWith("Artist:")) info.Artist = line.Split(new char[]{':'}, 2)[1];
+                if (line.StartsWith("ArtistUnicode:")) info.ArtistUnicode = line.Split(new char[]{':'}, 2)[1];
+                if (line.StartsWith("Version:")) info.Version = line.Split(new char[]{':'}, 2)[1];
+                if (line.StartsWith("Source:")) info.Source = line.Split(new char[]{':'}, 2)[1];
+                if (line.StartsWith("Tags:")) info.Tags = line.Split(new char[]{':'}, 2)[1].Split(' ');
+                if (line.StartsWith("BeatmapID:")) info.BeatmapID = int.Parse(line.Split(new char[] {':'}, 2)[1]);
+                if (line.StartsWith("BeatmapSetID:")) info.BeatmapSetID = int.Parse(line.Split(new char[] {':'}, 2)[1]);
+            }else if (block == "[Difficulty]")
+            {
+                if (line.StartsWith("CircleSize:")) info.CircleSize = int.Parse(line.Split(new char[]{':'}, 2)[1]);
+                if (line.StartsWith("ApproachRate:")) info.ApproachRate = int.Parse(line.Split(new char[]{':'}, 2)[1]);
+            }
+        }
+        return info;
+    }
+}
--- Assets/Scripts/Starter.cs
+++ Assets/Scripts/Starter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -12,11 +13,15 @@ using UnityEngine.UI;
 public static class StaticClass {
     public static AudioClip Bgm { get; set; }
     public static TextAsset Script { get; set; }
+    
+    public static OsuLoader Loader { get; set; }
     public static bool Auto { get; set; } = false;
     
     public static string Name { get; set; }
 
     public static float Sensibility { get; set; } = 0f;
+
+    public static bool IsOsu { get; set; } = false;
 }
 
 
@@ -35,6 +40,19 @@ public class Starter : MonoBehaviour
 
     public Slider sens;
     // public bool auto = false;
+    private Dictionary<int, OsuLoader> _loaderDict;
+
+    private string _makeStar(int h)
+    {
+        const string s = "★";
+        var startString = "";
+        for (var j = 0; j < h; j++)
+        {
+            startString += s;
+        }
+
+        return startString;
+    }
 
     void Start()
     {
@@ -43,17 +61,11 @@ public class Starter : MonoBehaviour
         {
             var b = Instantiate(firstButton, parent, true);
             b.transform.Find("Name").GetComponent<Text>().text = names[i];
-            const string s = "★";
-            var startString = "";
-            for (var j = 0; j < hardness[i]; j++)
-            {
-                startString += s;
-            }
-
-            b.transform.Find("Star").GetComponent<Text>().text = startString;
+            
+            b.transform.Find("Star").GetComponent<Text>().text = _makeStar(hardness[i]);
             var pos = b.transform.position;
             b.transform.position = new Vector3(pos.x, pos.y - i * 240, pos.z);
-            b.transform.Find("Image").GetComponent<RawImage>().texture = images[i];
+            b.transform.Find("Mask").Find("Image").GetComponent<RawImage>().texture = images[i];
             if (PlayerPrefs.HasKey("high-score-" + names[i]))
             {
                 var score = PlayerPrefs.GetFloat("high-score-" + names[i]);
@@ -66,6 +78,53 @@ public class Starter : MonoBehaviour
             
             b.GetComponent<Button>().onClick.AddListener(StartGame);
         }
+        
+        
+        const string fullPath = "Assets/Resources/data/";  
+        
+        _loaderDict = new Dictionary<int, OsuLoader>();
+        
+        //获取指定路径下面的所有资源文件  
+        if (Directory.Exists(fullPath))
+        {
+            var direction = new DirectoryInfo(fullPath);
+            var dirs = direction.GetDirectories();
+            
+            for (var j = 0; j < dirs.Length; j++)
+            {
+                var dir = dirs[j];
+                // 这是一首曲子
+                var image = dir.GetFiles("*.jpg")[0];
+                var osuFiles = dir.GetFiles("*.osu");
+                var osuLoader = new OsuLoader(osuFiles[0]);
+                var info = osuLoader.ReadSongInfo();
+                
+                
+                var b = Instantiate(firstButton, parent, true);
+                // 需要标记
+                _loaderDict[b.GetInstanceID()] = osuLoader;
+                b.transform.Find("Name").GetComponent<Text>().text = info.TitleUnicode;
+                b.transform.Find("Star").GetComponent<Text>().text = _makeStar((info.ApproachRate + info.CircleSize) / 2);
+                var pos = b.transform.position;
+                b.transform.position = new Vector3(pos.x, pos.y - (names.Length + j) * 240, pos.z);
+                var tex = new Texture2D(1, 1);
+                tex.LoadImage(File.ReadAllBytes(image.FullName));
+                var img = b.transform.Find("Mask").Find("Image");
+                img.GetComponent<RawImage>().texture = tex;
+                img.GetComponent<AspectRatioFitter>().aspectRatio = (float)tex.width / tex.height;
+                if (PlayerPrefs.HasKey("high-score-" + info.BeatmapID))
+                {
+                    var score = PlayerPrefs.GetFloat("high-score-" + info.BeatmapID);
+                    b.transform.Find("Score").GetComponent<Text>().text = Convert.ToInt32(score).ToString().PadLeft(7, '0');
+                }else
+                {
+                    b.transform.Find("Score").GetComponent<Text>().text = "0000000";
+                }
+            
+            
+                b.GetComponent<Button>().onClick.AddListener(StartGame);
+            }
+        }
 
         var rect = parent.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 300 + names.Length * 240);
     }
@@ -73,23 +132,43 @@ public class Starter : MonoBehaviour
     private void StartGame()
     {
         var obj = EventSystem.current.currentSelectedGameObject;
-        var n = obj.transform.Find("Name").GetComponent<Text>().text;
-        for (var i = 0; i < names.Length; i++)
+
+        if (_loaderDict.ContainsKey(obj.GetInstanceID()))
         {
-            if (names[i] == n)
+            StaticClass.Bgm = null;
+            StaticClass.Script = null;
+            StaticClass.Auto = autoToggle.isOn;
+            StaticClass.Sensibility = sens.value;
+            StaticClass.Name = null;
+            StaticClass.IsOsu = true;
+            StaticClass.Loader = _loaderDict[obj.GetInstanceID()];
+            PlayerPrefs.SetFloat("sens", StaticClass.Sensibility);
+            SceneManager.LoadScene("Music");
+        }
+        else
+        {
+            var n = obj.transform.Find("Name").GetComponent<Text>().text;
+            for (var i = 0; i < names.Length; i++)
             {
-                var script = scripts[i];
-                var bgm = bgms[i];
-                // 开始
-                StaticClass.Bgm = bgm;
-                StaticClass.Script = script;
-                StaticClass.Auto = autoToggle.isOn;
-                StaticClass.Sensibility = sens.value;
-                StaticClass.Name = n;
-                PlayerPrefs.SetFloat("sens", StaticClass.Sensibility);
-                SceneManager.LoadScene("Music");
-                break;
+                if (names[i] == n)
+                {
+                    var script = scripts[i];
+                    var bgm = bgms[i];
+                    // 开始
+                    StaticClass.Bgm = bgm;
+                    StaticClass.Script = script;
+                    StaticClass.Auto = autoToggle.isOn;
+                    StaticClass.Sensibility = sens.value;
+                    StaticClass.Name = n;
+                    StaticClass.IsOsu = false;
+                    StaticClass.Loader = null;
+                    PlayerPrefs.SetFloat("sens", StaticClass.Sensibility);
+                    SceneManager.LoadScene("Music");
+                    break;
+                }
             }
         }
+        
+        
     }
 }
