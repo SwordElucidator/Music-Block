e77398c5321affc9c3c7365c1dff074c1717605c
Hotaru <nagisa940216@gmail.com>
2020-09-25 17:54:41 +0800

    array / arraylist

--- Assets/Scripts/OsuLoader.cs
+++ Assets/Scripts/OsuLoader.cs
@@ -1,6 +1,7 @@
 ﻿
 
 using System;
+using System.Collections;
 using System.IO;
 
 public struct SongInfo
@@ -20,6 +21,16 @@ public struct SongInfo
     public int ApproachRate;
     public int BeatmapID;
     public int BeatmapSetID;
+    
+    // 扩展部分
+    public string AudioFilename;
+    public int AudioLeadIn;
+    public int PreviewTime;
+    public int Mode;
+    
+    // 变速部分todo
+
+    public ArrayList Notes;
 }
 
 
@@ -31,6 +42,59 @@ public class OsuLoader
     {
         _file = file;
     }
+
+
+    private static string _readLine(TextReader reader, string block, SongInfo info)
+    {
+        var line = reader.ReadLine();
+        if (line == null) return block;
+        if (line.Length >= 2 && line[0] == '[' && line[line.Length - 1] == ']')
+        {
+            return line;
+        }
+
+        if (block == "[General]")
+        {
+            if (line.StartsWith("AudioFilename:")) info.AudioFilename = line.Split(new char[]{':'}, 2)[1];
+            if (line.StartsWith("AudioLeadIn:")) info.AudioLeadIn = int.Parse(line.Split(new char[] {':'}, 2)[1]);
+            if (line.StartsWith("PreviewTime:")) info.PreviewTime = int.Parse(line.Split(new char[] {':'}, 2)[1]);
+            if (line.StartsWith("Mode:")) info.Mode = int.Parse(line.Split(new char[] {':'}, 2)[1]);
+        }
+        else if (block == "[Metadata]")
+        {
+            if (line.StartsWith("Title:")) info.Title = line.Split(new char[]{':'}, 2)[1];
+            if (line.StartsWith("TitleUnicode:")) info.TitleUnicode = line.Split(new char[]{':'}, 2)[1];
+            if (line.StartsWith("Artist:")) info.Artist = line.Split(new char[]{':'}, 2)[1];
+            if (line.StartsWith("ArtistUnicode:")) info.ArtistUnicode = line.Split(new char[]{':'}, 2)[1];
+            if (line.StartsWith("Version:")) info.Version = line.Split(new char[]{':'}, 2)[1];
+            if (line.StartsWith("Source:")) info.Source = line.Split(new char[]{':'}, 2)[1];
+            if (line.StartsWith("Tags:")) info.Tags = line.Split(new char[]{':'}, 2)[1].Split(' ');
+            if (line.StartsWith("BeatmapID:")) info.BeatmapID = int.Parse(line.Split(new char[] {':'}, 2)[1]);
+            if (line.StartsWith("BeatmapSetID:")) info.BeatmapSetID = int.Parse(line.Split(new char[] {':'}, 2)[1]);
+        }else if (block == "[Difficulty]")
+        {
+            if (line.StartsWith("CircleSize:")) info.CircleSize = int.Parse(line.Split(new char[]{':'}, 2)[1]);
+            if (line.StartsWith("ApproachRate:")) info.ApproachRate = int.Parse(line.Split(new char[]{':'}, 2)[1]);
+        }else if (block == "[HitObjects]")
+        {
+            info.Notes.Add(int.Parse(line.Split(',')[2]));
+        }
+        return block;
+    }
+    
+    public SongInfo DeepRead()
+    {
+        var reader = _file.OpenText();
+        var block = "";
+        var info = new SongInfo();
+        info.Notes = new ArrayList();
+        while (!reader.EndOfStream)
+        {
+            block = _readLine(reader, block, info);
+        }
+        reader.Close();
+        return info;
+    }
     
 
     public SongInfo ReadSongInfo()
@@ -40,31 +104,10 @@ public class OsuLoader
         var info = new SongInfo();
         while (!reader.EndOfStream)
         {
-            var line = reader.ReadLine();
-            if (line == null) continue;
-            if (line.Length >= 2 && line[0] == '[' && line[line.Length - 1] == ']')
-            {
-                block = line;
-                if (block == "[Events]") break;
-                continue;
-            }
-            if (block == "[Metadata]")
-            {
-                if (line.StartsWith("Title:")) info.Title = line.Split(new char[]{':'}, 2)[1];
-                if (line.StartsWith("TitleUnicode:")) info.TitleUnicode = line.Split(new char[]{':'}, 2)[1];
-                if (line.StartsWith("Artist:")) info.Artist = line.Split(new char[]{':'}, 2)[1];
-                if (line.StartsWith("ArtistUnicode:")) info.ArtistUnicode = line.Split(new char[]{':'}, 2)[1];
-                if (line.StartsWith("Version:")) info.Version = line.Split(new char[]{':'}, 2)[1];
-                if (line.StartsWith("Source:")) info.Source = line.Split(new char[]{':'}, 2)[1];
-                if (line.StartsWith("Tags:")) info.Tags = line.Split(new char[]{':'}, 2)[1].Split(' ');
-                if (line.StartsWith("BeatmapID:")) info.BeatmapID = int.Parse(line.Split(new char[] {':'}, 2)[1]);
-                if (line.StartsWith("BeatmapSetID:")) info.BeatmapSetID = int.Parse(line.Split(new char[] {':'}, 2)[1]);
-            }else if (block == "[Difficulty]")
-            {
-                if (line.StartsWith("CircleSize:")) info.CircleSize = int.Parse(line.Split(new char[]{':'}, 2)[1]);
-                if (line.StartsWith("ApproachRate:")) info.ApproachRate = int.Parse(line.Split(new char[]{':'}, 2)[1]);
-            }
+            block = _readLine(reader, block, info);
+            if (block == "[Events]") break;
         }
+        reader.Close();
         return info;
     }
 }
